eb4ce503e7169ad79c93d99f4ee9193f3b247f57
Jacob50657 <251888@cursist.rockopnh.nl>
2019-03-14 12:10:10 +0100

    Version 1.1

--- html/index.php
+++ html/index.php
@@ -11,19 +11,34 @@
     <form method="get">
       <!--Question One-->
       <input type="text" name="Qone" value="">
-      <input type="text" name="ansOne" value="" style="background-color:red;">
+      <input type="text" name="ansOne" value="">
       <!--Question Two-->
       <input type="text" name="Qtwo" value="">
-      <input type="text" name="ansTwo" value="" style="background-color:blue;">
+      <input type="text" name="ansTwo" value="">
       <!--Question Three-->
       <input type="text" name="Qthree" value="">
-      <input type="text" name="ansThree" value="" style="background-color:yellow;">
+      <input type="text" name="ansThree" value="">
       <!--Question Four-->
       <input type="text" name="Qfour" value="">
-      <input type="text" name="ansFour" value="" style="background-color:purple;">
+      <input type="text" name="ansFour" value="">
       <!--Question Five-->
       <input type="text" name="Qfive" value="">
-      <input type="text" name="ansFive" value="" style="background-color:green;">
+      <input type="text" name="ansFive" value="">
+      <!--Question Six-->
+      <input type="text" name="Qsix" value="">
+      <input type="text" name="ansSix" value="">
+      <!--Question Seven-->
+      <input type="text" name="Qseven" value="">
+      <input type="text" name="ansSeven" value="">
+      <!--Question Eight-->
+      <input type="text" name="Qeight" value="">
+      <input type="text" name="ansEight" value="">
+      <!--Question Nine-->
+      <input type="text" name="Qnine" value="">
+      <input type="text" name="ansNine" value="">
+      <!--Question Ten-->
+      <input type="text" name="Qten" value="">
+      <input type="text" name="ansTen" value="">
 
       <button name="btn">press</button>
       <button name="delete">delete</button>
@@ -32,30 +47,56 @@
 
       if(!isset($_GET['btn'])){
       }else{
-        $Q1= $_GET['Qone'];
-        $_SESSION['Q1']=$Q1;
-        $A1= $_GET['ansOne'];
-        $_SESSION['A1']=$A1;
+        // Question 1 //
+        if(isset($_GET['Qone']) && isset($_GET['ansOne'])){
+        $_SESSION['Q1']=$_GET['Qone'];
+        $_SESSION['A1']=$_GET['ansOne'];
+        }
         // Question 2 //
-        $Q2= $_GET['Qtwo'];
-        $_SESSION['Q2']=$Q2;
-        $A2= $_GET['ansTwo'];
-        $_SESSION['A2']=$A2;
+        if(isset($_GET['Qtwo']) && isset($_GET['ansTwo'])){
+        $_SESSION['Q2']=$_GET['Qtwo'];
+        $_SESSION['A2']=$_GET['ansTwo'];
+        }
         // Question 3 //
-        $Q3= $_GET['Qthree'];
-        $_SESSION['Q3']=$Q3;
-        $A3= $_GET['ansThree'];
-        $_SESSION['A3']=$A3;
+        if(isset($_GET['Qthree']) && isset($_GET['ansThree'])){
+        $_SESSION['Q3']=$_GET['Qthree'];
+        $_SESSION['A3']=$_GET['ansThree'];
+        }
         // Question 4 //
-        $Q4= $_GET['Qfour'];
-        $_SESSION['Q4']=$Q4;
-        $A4= $_GET['ansFour'];
-        $_SESSION['A4']=$A4;
+        if(isset($_GET['Qfour']) && isset($_GET['ansFour'])){
+        $_SESSION['Q4']=$_GET['Qfour'];
+        $_SESSION['A4']=$_GET['ansFour'];
+        }
         // Question 5 //
-        $Q5= $_GET['Qfive'];
-        $_SESSION['Q5']=$Q5;
-        $A5= $_GET['ansFive'];
-        $_SESSION['A5']=$A5;
+        if(isset($_GET['Qfive']) && isset($_GET['ansFive'])){
+        $_SESSION['Q5']=$_GET['Qfive'];
+        $_SESSION['A5']=$_GET['ansFive'];
+        }
+        // Question 6 //
+        if(isset($_GET['Qsix']) && isset($_GET['ansSix'])){
+        $_SESSION['Q6']=$_GET['Qsix'];
+        $_SESSION['A6']=$_GET['ansSix'];
+        }
+        // Question 7 //
+        if(isset($_GET['Qseven']) && isset($_GET['ansSeven'])){
+        $_SESSION['Q7']=$_GET['Qseven'];
+        $_SESSION['A7']=$_GET['ansSeven'];
+        }
+        // Question 8 //
+        if(isset($_GET['Qeight']) && isset($_GET['ansEight'])){
+        $_SESSION['Q8']=$_GET['Qeight'];
+        $_SESSION['A8']=$_GET['ansEight'];
+        }
+        // Question 9 //
+        if(isset($_GET['Qnine']) && isset($_GET['ansNine'])){
+        $_SESSION['Q9']=$_GET['Qnine'];
+        $_SESSION['A9']=$_GET['ansNine'];
+        }
+        // Question 10 //
+        if(isset($_GET['Qten']) && isset($_GET['ansTen'])){
+        $_SESSION['Q10']=$_GET['Qten'];
+        $_SESSION['A10']=$_GET['ansTen'];
+        }
       }
 
       if(isset($_GET['delete'])){
--- html/pagina2.php
+++ html/pagina2.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$i = "";
  ?>
 <html lang="en" dir="ltr">
   <head>
@@ -10,115 +11,257 @@ session_start();
     <form method="get">
       <!--Question 1-->
       <p><?php
-      if($_SESSION['Q1']){
-      echo "you didn't fill anything in.";
+      if(!isset($_SESSION['Q1']) || ($_SESSION['Q1'] == "")){
+      echo "";
       }else{
       echo 'vraag 1 : '.$_SESSION['Q1'];
       }?></p>
-      <input type="text" name="fillOne" value="">
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q1'] !== "")
+      echo '<input type="text" name="fillOne" value="">';
+      ?>
       <!--Question 2-->
       <p><?php
-      if($_SESSION['Q2']){
-      echo "you didn't fill anything in.";
+      if(!isset($_SESSION['Q2']) || ($_SESSION['Q2'] == "")){
+      echo "";
       }else{
       echo 'vraag 2 : '.$_SESSION['Q2'];
       }?></p>
-      <input type="text" name="fillTwo" value="">
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q2'] !== "")
+      echo '<input type="text" name="fillTwo" value="">';
+      ?>
+
       <!--Question 3-->
       <p><?php
-      if(!isset($_SESSION['Q3'])){
-      echo "you didn't fill anything in.";
+      if(!isset($_SESSION['Q3']) || ($_SESSION['Q3'] == "")){
+      echo "";
       }else{
       echo 'vraag 3 : '.$_SESSION['Q3'];
       }?></p>
-      <input type="text" name="fillThree" value="">
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q3'] !== "")
+      echo '<input type="text" name="fillThree" value="">';
+      ?>
+
       <!--Question 4-->
       <p><?php
-      if(!isset($_SESSION['Q4'])){
-      echo "you didn't fill anything in.";
+      // als er een vraag is.
+      if(!isset($_SESSION['Q4']) || ($_SESSION['Q4'] == "")){
+      echo "";
       }else{
       echo 'vraag 4 : '.$_SESSION['Q4'];
       }?></p>
-      <input type="text" name="fillFour" value="">
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q4'] !== "")
+      echo '<input type="text" name="fillFour" value="">';
+      ?>
+
       <!--Question 5-->
       <p><?php
-      if(!isset($_SESSION['Q5'])){
-      echo "you didn't fill anything in.";
+      if(!isset($_SESSION['Q5']) || ($_SESSION['Q5'] == "")){
+      echo "";
       }else{
       echo 'vraag 5 : '.$_SESSION['Q5'];
       }?></p>
-      <input type="text" name="fillFive" value="">
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q5'] !== "")
+      echo '<input type="text" name="fillFive" value="">';
+      ?>
+
+      <!--Question 6-->
+      <p><?php
+      if(!isset($_SESSION['Q6']) || ($_SESSION['Q6'] == "")){
+      echo "";
+      }else{
+      echo 'vraag 6 : '.$_SESSION['Q6'];
+      }?></p>
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q6'] !== "")
+      echo '<input type="text" name="fillSix" value="">';
+      ?>
+
+      <!--Question 7-->
+      <p><?php
+      if(!isset($_SESSION['Q7']) || ($_SESSION['Q7'] == "")){
+      echo "";
+      }else{
+      echo 'vraag 7 : '.$_SESSION['Q7'];
+      }?></p>
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q7'] !== "")
+      echo '<input type="text" name="fillSeven" value="">';
+      ?>
+
+      <!--Question 8-->
+      <p><?php
+      if(!isset($_SESSION['Q8']) || ($_SESSION['Q8'] == "")){
+      echo "";
+      }else{
+      echo 'vraag 8 : '.$_SESSION['Q8'];
+      }?></p>
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q8'] !== "")
+      echo '<input type="text" name="fillEight" value="">';
+      ?>
+
+      <!--Question 9-->
+      <p><?php
+      if(!isset($_SESSION['Q9']) || ($_SESSION['Q9'] == "")){
+      echo "";
+      }else{
+      echo 'vraag 9 : '.$_SESSION['Q9'];
+      }?></p>
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q9'] !== "")
+      echo '<input type="text" name="fillNine" value="">';
+      ?>
+
+      <!--Question 10-->
+      <p><?php
+      if(!isset($_SESSION['Q10']) || ($_SESSION['Q10'] == "")){
+      echo "";
+      }else{
+      echo 'vraag 10 : '.$_SESSION['Q10'];
+      }?></p>
+      <?php
+      // als er een vraag is
+      if($_SESSION['Q10'] !== "")
+      echo '<input type="text" name="fillTen" value="">';
+      ?>
 
 
       <button name="press">press</button>
     </form>
     <?php
-
     $punten = 0;
 
     // Question 1 //
     if(isset($_GET['press'])){
     if($_GET['fillOne'] == $_SESSION['A1']){
     $punten++;
-    }
     }else{}
-    // Question 1 //
+    }else{}
+    // Question 2 //
     if(isset($_GET['press'])){
     if($_GET['fillTwo'] == $_SESSION['A2']){
     $punten++;
     }
     }else{}
-    // Question 1 //
+    // Question 3 //
     if(isset($_GET['press'])){
-    if(isset($_GET['fillThree']) == $_SESSION['A3']){
+    if($_GET['fillThree'] == $_SESSION['A3']){
+    $punten++;
+    }
+    }else{}
+    // Question 4 //
+    if(isset($_GET['press'])){
+    if($_GET['fillFour'] == $_SESSION['A4']){
+    $punten++;
+    }
+    }else{}
+    // Question 5 //
+    if(isset($_GET['press'])){
+    if($_GET['fillFive'] == $_SESSION['A5']){
+    $punten++;
+    }
+    }else{}
+    // Question 6 //
+    if(isset($_GET['press'])){
+    if($_GET['fillSix'] == $_SESSION['A6']){
+    $punten++;
+    }
+    }else{}
+    // Question 7 //
+    if(isset($_GET['press'])){
+    if($_GET['fillSeven'] == $_SESSION['A7']){
     $punten++;
     }else{}
     }else{}
-    // Question 1 //
+    // Question 8 //
     if(isset($_GET['press'])){
-    if(isset($_GET['fillFour']) == $_SESSION['A4']){
+    if($_GET['fillEight'] == $_SESSION['A8']){
     $punten++;
     }else{}
     }else{}
-    // Question 1 //
+    // Question 9 //
     if(isset($_GET['press'])){
-    if(isset($_GET['fillFive']) == $_SESSION['A5']){
+    if($_GET['fillNine'] == $_SESSION['A9']){
     $punten++;
     }else{}
+    }else{}
+    // Question 10 //
+    if(isset($_GET['press'])){
+    if($_GET['fillTen'] == $_SESSION['A10']){
+    $punten++;
+    }
     }else{}
     ?>
     <?php
     switch ($punten) {
-      case 1:{
-        echo "je hebt 0 vragen goed oof";
+      case 1:
+      {
+        echo "je hebt 10% goed";
         break;
       }
       case 2:
       {
-        echo "je hebt 1 vraag goed, Noob";
+        echo "je hebt 20% goed";
         break;
       }
       case 3:
       {
-        echo "je hebt 2 vragen goed, Vegan";
-        break;
-      }
-      case 4:
-      {
-      echo "je hebt 3 vragen goed, Probeer nog een keer";
+      echo "je hebt 30% goed";
       break;
     }
+      case 4:
+      {
+        echo "je hebt 40% goed";
+        break;
+      }
       case 5:
       {
-        echo "je hebt 1 fout, Goed";
+        echo "je hebt 50% goed";
         break;
       }
       case 6:
       {
-        echo "je alles goed, Perfect";
+        echo "je hebt 60% goed";
         break;
       }
-      default;
+      case 7:
+      {
+        echo "je hebt 70% goed";
+        break;
+      }
+      case 8:
+      {
+        echo "je hebt 80% goed";
+        break;
+      }
+      case 9:
+      {
+        echo "je hebt 90% goed";
+        break;
+      }
+      case 10:
+      {
+        echo "je alles goed, 100%!";
+        break;
+      }
+      default:
+      {
+        echo "je hebt 0% goed.";
+      }
       break;
       }
      ?>
