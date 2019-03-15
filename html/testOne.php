ca1d19f288ac02c8915473c6632919590071ac0f
Jacob50657 <251888@cursist.rockopnh.nl>
2019-03-15 10:25:09 +0100

    Version 1.3

--- html/testOne.php
+++ html/testOne.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$i = "";
  ?>
 <html lang="en" dir="ltr">
   <head>
@@ -16,15 +15,19 @@ $i = "";
     <form method="get">
       <!--Question 1-->
       <?php
+      if(!isset($_GET['press'])){
+      $disApp = "";
+      }else{$disApp = 'hidden="false"';
+      }
       if(!isset($_SESSION['Q1']) || ($_SESSION['Q1'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q1']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q1'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q1'] !== "")
-      echo '<input type="text" class="fill" name="fillOne" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillOne" value="">';
       ?>
 
       <!--Question 2-->
@@ -32,12 +35,12 @@ $i = "";
       if(!isset($_SESSION['Q2']) || ($_SESSION['Q2'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q2']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q2'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q2'] !== "")
-      echo '<input type="text" class="fill" name="fillTwo" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillTwo" value="">';
       ?>
 
       <!--Question 3-->
@@ -45,12 +48,12 @@ $i = "";
       if(!isset($_SESSION['Q3']) || ($_SESSION['Q3'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q3']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q3'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q3'] !== "")
-      echo '<input type="text" class="fill" name="fillThree" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillThree" value="">';
       ?>
 
       <!--Question 4-->
@@ -59,12 +62,12 @@ $i = "";
       if(!isset($_SESSION['Q4']) || ($_SESSION['Q4'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q4']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q4'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q4'] !== "")
-      echo '<input type="text" class="fill" name="fillFour" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillFour" value="">';
       ?>
 
       <!--Question 5-->
@@ -72,12 +75,12 @@ $i = "";
       if(!isset($_SESSION['Q5']) || ($_SESSION['Q5'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q5']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q5'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q5'] !== "")
-      echo '<input type="text" class="fill" name="fillFive" value="">';
+      echo '<input type="text" class="fill"  autocomplete="off"name="fillFive" value="">';
       ?>
 
       <!--Question 6-->
@@ -85,12 +88,12 @@ $i = "";
       if(!isset($_SESSION['Q6']) || ($_SESSION['Q6'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q6']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q6'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q6'] !== "")
-      echo '<input type="text" class="fill" name="fillSix" value="">';
+      echo '<input type="text" class="fill"  autocomplete="off"name="fillSix" value="">';
       ?>
 
       <!--Question 7-->
@@ -98,12 +101,12 @@ $i = "";
       if(!isset($_SESSION['Q7']) || ($_SESSION['Q7'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q7']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q7'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q7'] !== "")
-      echo '<input type="text" class="fill" name="fillSeven" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillSeven" value="">';
       ?>
 
       <!--Question 8-->
@@ -111,12 +114,12 @@ $i = "";
       if(!isset($_SESSION['Q8']) || ($_SESSION['Q8'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q8']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q8'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q8'] !== "")
-      echo '<input type="text" class="fill" name="fillEight" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillEight" value="">';
       ?>
 
       <!--Question 9-->
@@ -124,12 +127,12 @@ $i = "";
       if(!isset($_SESSION['Q9']) || ($_SESSION['Q9'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q9']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q9'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q9'] !== "")
-      echo '<input type="text" class="fill" name="fillNine" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillNine" value="">';
       ?>
 
       <!--Question 10-->
@@ -137,141 +140,141 @@ $i = "";
       if(!isset($_SESSION['Q10']) || ($_SESSION['Q10'] == "")){
       echo "";
       }else{
-      echo "<input type='text' class='qn' readonly='true' value=".$_SESSION['Q10']."?>";
+      echo "<input type='text' class='qn' disabled='disabled' value=".$_SESSION['Q10'].">";
       }?>
       <?php
       // als er een vraag is
       if($_SESSION['Q10'] !== "")
-      echo '<input type="text" class="fill" name="fillTen" value="">';
+      echo '<input type="text" class="fill" autocomplete="off" name="fillTen" value="">';
       ?>
 
 
-      <button name="press" class="btn">press</button>
+      <button name="press" class="btn"<?php echo $disApp; ?>><p>Nakijken</p></button>
     </form>
     <?php
     $punten = 0;
-
-    // Question 1 //
+    // Check //
     if(isset($_GET['press'])){
+    // Question 1 //
+    if(isset($_GET['fillOne']) && isset($_SESSION['A1'])){
     if($_GET['fillOne'] == $_SESSION['A1']){
     $punten++;
-    }else{}
-    }else{}
+    }
+    }
     // Question 2 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillTwo']) && isset($_SESSION['A2'])){
     if($_GET['fillTwo'] == $_SESSION['A2']){
     $punten++;
     }
-    }else{}
+    }
     // Question 3 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillThree']) && isset($_SESSION['A3'])){
     if($_GET['fillThree'] == $_SESSION['A3']){
     $punten++;
     }
-    }else{}
+    }
     // Question 4 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillFour']) && isset($_SESSION['A4'])){
     if($_GET['fillFour'] == $_SESSION['A4']){
     $punten++;
     }
-    }else{}
+    }
     // Question 5 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillFive']) && isset($_SESSION['A5'])){
     if($_GET['fillFive'] == $_SESSION['A5']){
     $punten++;
     }
-    }else{}
+    }
     // Question 6 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillSix']) && isset($_SESSION['A6'])){
     if($_GET['fillSix'] == $_SESSION['A6']){
     $punten++;
     }
-    }else{}
+    }
     // Question 7 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillSeven']) && isset($_SESSION['A7'])){
     if($_GET['fillSeven'] == $_SESSION['A7']){
     $punten++;
-    }else{}
-    }else{}
+    }
+    }
     // Question 8 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillEight']) && isset($_SESSION['A8'])){
     if($_GET['fillEight'] == $_SESSION['A8']){
     $punten++;
-    }else{}
-    }else{}
+    }
+    }
     // Question 9 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillNine']) && isset($_SESSION['A9'])){
     if($_GET['fillNine'] == $_SESSION['A9']){
     $punten++;
-    }else{}
-    }else{}
+    }
+    }
     // Question 10 //
-    if(isset($_GET['press'])){
+    if(isset($_GET['fillTen']) && isset($_SESSION['A10'])){
     if($_GET['fillTen'] == $_SESSION['A10']){
     $punten++;
     }
-    }else{}
-    ?>
-    <?php
+    }
     switch ($punten) {
       case 1:
       {
-        echo '<div class="punten"><p>je hebt 10% goed</p></div>';
+        echo '<input type="text" class="punten" value="je hebt 10% goed">';
         break;
       }
       case 2:
       {
-        echo '<div class="punten"><p>je hebt 20% goed</p></div>';
+        echo '<input type="text" class="punten" value="je hebt 20% goed">';
         break;
       }
       case 3:
       {
-      echo '<div class="punten"><p>je hebt 30% goed</p></div>';
+      echo '<input type="text" class="punten" value="je hebt 30% goed">';
       break;
     }
       case 4:
       {
-        echo '<div class="punten"><p>je hebt 40% goed</p></div>';
+        echo '<input type="text" class="punten" value="je hebt 40% goed">';
         break;
       }
       case 5:
       {
-        echo '<div class="punten"><p>je hebt 50% goed</p></div>';
+        echo '<input type="text" class="punten" value="je hebt 50% goed">';
         break;
       }
       case 6:
       {
-        echo '<div class="punten"><p>je hebt 60% goed</p></div>';
+        echo '<input type="text" class="punten" value="je hebt 60% goed">';
         break;
       }
       case 7:
       {
-        echo '<div class="punten"><p>je hebt 70% goed</p></div>';
+        echo '<input type="text" class="punten" value="je hebt 70% goed">';
         break;
       }
       case 8:
       {
-        echo '<div class="punten"><p>je hebt 80% goed</p></div>';
+        echo '<input type="text" class="punten" value="je hebt 80% goed">';
         break;
       }
       case 9:
       {
-        echo '<div class="punten"><p>je hebt 90% goed</p></div>';
+        echo '<input type="text" class="punten" disabled="true" value="je hebt 90% goed">';
         break;
       }
       case 10:
       {
-        echo '<div class="punten"><p>je hebt alles goed, 100%!</p></div>';
+        echo '<input type="text" class="punten" value="je hebt alles goed, 100%!">';
         break;
       }
       default:
       {
-        echo '<div class="punten"><p>je hebt 0% goed</p></div>';
+        echo '<input type="text" class="punten" disabled="true" value="je hebt 0% goed">';
       }
       break;
       }
+      echo '<a href="index.php" class="terug">terug</a>';
+      }
      ?>
-     <a href="index.php" class="terug">terug</a>
    </div>
   </body>
 </html>
